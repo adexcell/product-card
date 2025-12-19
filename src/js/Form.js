bd500dab29dba290323db03d1a7b05dc85df9d47
abakar_aliev <salachali@gmail.com>
2025-12-19 19:52:10 +0500

    add modal.js, form.js, rewrite homework-9 logic

--- src/js/Form.js
+++ src/js/Form.js
@@ -1,42 +1,22 @@
 export class Form {
     constructor(form_id) {
         this.form = document.getElementById(form_id); 
-        this.inputList = Array.from(this.form.querySelectorAll('.form_input'))
         this.button = this.form.querySelector('.button')
-        this.data = this.getData()
-        
     }
 
     getData() {
-        let data;
-        this.form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            const formData = new FormData(event.target);
-            data = Object.fromEntries(formData.entries());
-        })
-        return data
+        return Object.fromEntries(new FormData(this.form))
     }
 
-    isValidity() {
-        return this.inputList.every((input) => input.checkValidity())
+    isValid() {
+        return this.form.checkValidity()
     }
 
     reset() {
         this.form.reset();
     }
-}
 
-export class FormSignUp extends Form {
-    constructor(form_id) {
-        super(form_id)
-    }
-
-    checkPasswords() {
-        if (this.data.password !== this.data.repeatPassword) {
-            alert("wrong password")
-        }
-        this.data.createdOn = new Date()
-        console.log(this.data)
+    initButton() {
+        this.button.addEventListener('click')
     }
 }
-
